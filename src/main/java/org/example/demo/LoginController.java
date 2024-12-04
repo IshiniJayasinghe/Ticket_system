8e336e2ff64a213d555c4f1b7db69350111092a4
IshiniJayasinghe <ishinijayasinghe29@gmail.com>
2024-12-05 01:43:54 +0530

    started the project and created configuration class.

---
+++ src/main/java/org/example/demo/LoginController.java
@@ -0,0 +1,17 @@
+package org.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Label Name;
+
+    @FXML
+    private Label Password;
+
+
+}
